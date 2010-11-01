4e0141ffae64b8c89f798670c2d278114d2f0c2f
danielsc99 <danielsc99@e724f736-04db-794b-b648-ee64ee4c4e6e>
2010-11-01 00:14:54 +0000

    expire_time no guild_storage
    git-svn-id: https://test.cortezonline.org/svn/Cortez/eA/trunk@393 e724f736-04db-794b-b648-ee64ee4c4e6e

--- _update.sql
+++ _update.sql
@@ -22,8 +22,6 @@ CREATE TABLE IF NOT EXISTS `mercenary_owner` (
   PRIMARY KEY  (`char_id`)
 );
 
-Alter Table `cart_inventory` Add `expire_time` int(11) unsigned NOT NULL default '0';
-
 ALTER TABLE `char` CHANGE `status_point` `status_point` INT( 11 ) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `char` CHANGE `skill_point` `skill_point` INT( 11 ) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `char` CHANGE `manner` `manner` SMALLINT ( 6 ) NOT NULL DEFAULT '0';
@@ -32,8 +30,6 @@ ALTER TABLE `char` ADD COLUMN `mer_id` INTEGER UNSIGNED NOT NULL DEFAULT '0' AFT
 
 ALTER TABLE `guild` MODIFY COLUMN `exp` bigint(20) unsigned NOT NULL default '0';
 
-Alter Table `inventory` Add `expire_time` int(11) unsigned NOT NULL default '0';
-
 Alter Table `quest` Change Column `state` `state` enum('0','1','2') NOT NULL default '0';
 Alter Table `quest` Add `time` int(11) unsigned NOT NULL default '0';
 Alter Table `quest` Add `count1` mediumint(8) unsigned NOT NULL default '0';
@@ -41,6 +37,9 @@ Alter Table `quest` Add `count2` mediumint(8) unsigned NOT NULL default '0';
 Alter Table `quest` Add `count3` mediumint(8) unsigned NOT NULL default '0';
 
 Alter Table `storage` Add `expire_time` int(11) unsigned NOT NULL default '0';
+Alter Table `inventory` Add `expire_time` int(11) unsigned NOT NULL default '0';
+Alter Table `cart_inventory` Add `expire_time` int(11) unsigned NOT NULL default '0';
+Alter Table `guild_storage` Add `expire_time` int(11) unsigned NOT NULL default '0';
 
 DELETE FROM `global_reg_value` WHERE `str`='COOKING_ATTEMPT' OR str='question$' OR str='question2$' OR str='question3$';
 
